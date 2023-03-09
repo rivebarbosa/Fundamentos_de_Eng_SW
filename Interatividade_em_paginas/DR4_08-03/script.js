1cfbd8225ef26d324fc0b6ee9384e18963f774f7
Roberto Rivelino <roberto.rivebarbosa@gmail.com>
2023-03-09 07:09:37 -0300

    DD4_08-03

--- Interatividade_em_paginas/DR4_08-03/script.js
+++ Interatividade_em_paginas/DR4_08-03/script.js
@@ -2,5 +2,13 @@ document.addEventListener("click", bang);
 
 function bang(){
     document.body.style.backgroundColor = "yellow";
-    document.body.innerHTML = "<h1>BANG!!!</h1>"
+    // document.body.innerHTML = "<h1>BANG!!!</h1>"
+    // document.body.innerHTML = "<button id='botao'>Clique aqui</button>"
+}
+
+let botaoBacana = document.querySelector("#botao");
+botaoBacana.addEventListener("click", showText);
+
+function showText(){
+    document.body.classList = "body"
 }
